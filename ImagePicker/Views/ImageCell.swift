37383db8568ebf65ce3802a9dc8c763c5ba0ed10
Tsering Lama <tseringlama@pursuit.org>
2020-01-22 17:27:30 -0500

    adding gesture for deletion of images

--- ImagePicker/Views/ImageCell.swift
+++ ImagePicker/Views/ImageCell.swift
@@ -12,14 +12,26 @@ class ImageCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    // longpressed recognizer
+    private lazy var longPressedGesture: UILongPressGestureRecognizer = {
+        let gesture = UILongPressGestureRecognizer()
+        gesture.addTarget(self, action: #selector(longPressedAction(gesture:)))
+        return gesture
+    }()
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         layer.cornerRadius = 20.0
         backgroundColor = .orange
+        addGestureRecognizer(longPressedGesture)
+    }
+    
+    @objc
+    private func longPressedAction(gesture: UILongPressGestureRecognizer) {
+        
     }
     
     public func configureCell(imageObject: ImageObject) {
-        
         // convering data to UIImage
         guard let image = UIImage(data: imageObject.imageData) else {
             return
