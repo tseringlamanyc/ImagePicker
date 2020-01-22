6c6616638db3f7ccb06f9b28390b7f300e8e38d1
Tsering Lama <tseringlama@pursuit.org>
2020-01-22 15:41:34 -0500

    wrote function to add photo from the photo library

--- ImagePicker/Controllers/ImagesViewController.swift
+++ ImagePicker/Controllers/ImagesViewController.swift
@@ -9,27 +9,54 @@
 import UIKit
 
 class ImagesViewController: UIViewController {
-  
-  @IBOutlet weak var collectionView: UICollectionView!
-      
-  private var imageObjects = [ImageObject]()
-  private var imagePickerController = UIImagePickerController()
     
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    collectionView.dataSource = self
-    collectionView.delegate = self
-  }
+    @IBOutlet weak var collectionView: UICollectionView!
     
+    private var imageObjects = [ImageObject]()
+    
+    private var imagePickerController = UIImagePickerController()
+    
+    private var selectedImage: UIImage? {
+        didSet {
+            appendNewPhoto()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        // set UIImagePickerController delegate
+        imagePickerController.delegate = self
+    }
+    
+    private func appendNewPhoto() {
+        //convert UIImage to data
+        guard let image = selectedImage, let imageData = image.jpegData(compressionQuality: 1.0) else {
+            return
+        }
+        // create an image object array
+        let imageObject = ImageObject(imageData: imageData, date: Date())
+        
+        // insert image
+        imageObjects.insert(imageObject, at: 0)
+        
+        // create an indexpath for insertion into collection view
+        let indexpath = IndexPath(row: 0, section: 0)
+        
+        // insert new cell into collection view
+        collectionView.insertItems(at: [indexpath])
+    }
     
     @IBAction func addPictureButton(_ sender: UIBarButtonItem) {
         // present an action sheet to the user
         // actions: camera, photo library or cancel
         
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (<#UIAlertAction#>) in
-            <#code#>
-        }
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default)
+        
         let photoLib = UIAlertAction(title: "Photo Library", style: .default) { [weak self] alertAction in
             self?.showImageController(isCameraSelected: false)
         }
@@ -37,7 +64,7 @@ class ImagesViewController: UIViewController {
         
         // check if camera is available
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-           alertController.addAction(cameraAction)
+            alertController.addAction(cameraAction)
         }
         
         alertController.addAction(photoLib)
@@ -59,37 +86,56 @@ class ImagesViewController: UIViewController {
     
 }
 
+extension ImagesViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // images comes in a form of a dictionary
+        // we need to access the UIImagePickerController.Infokey.original ket to get the UIImage that was selected
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        <#code#>
+    }
+    
+}
+
 // MARK: - UICollectionViewDataSource
 extension ImagesViewController: UICollectionViewDataSource {
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 50
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as? ImageCell else {
-      fatalError("could not downcast to an ImageCell")
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
     }
-    return cell
-  }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as? ImageCell else {
+            fatalError("could not downcast to an ImageCell")
+        }
+        return cell
+    }
 }
 
 // MARK: - UICollectionViewDelegateFlowLayout
 extension ImagesViewController: UICollectionViewDelegateFlowLayout {
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    let maxWidth: CGFloat = UIScreen.main.bounds.size.width // width of the device
-    let itemWidth: CGFloat = maxWidth * 0.80
-    return CGSize(width: itemWidth, height: itemWidth)  }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxWidth: CGFloat = UIScreen.main.bounds.size.width // width of the device
+        let itemWidth: CGFloat = maxWidth * 0.80
+        return CGSize(width: itemWidth, height: itemWidth)  }
 }
 
 // more here: https://nshipster.com/image-resizing/
 // MARK: - UIImage extension
 extension UIImage {
-  func resizeImage(to width: CGFloat, height: CGFloat) -> UIImage {
-    let size = CGSize(width: width, height: height)
-    let renderer = UIGraphicsImageRenderer(size: size)
-    return renderer.image { (context) in
-      self.draw(in: CGRect(origin: .zero, size: size))
+    func resizeImage(to width: CGFloat, height: CGFloat) -> UIImage {
+        let size = CGSize(width: width, height: height)
+        let renderer = UIGraphicsImageRenderer(size: size)
+        return renderer.image { (context) in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
     }
-  }
 }
 
